75e2168411f9e4076acced76358865839599c9e0
Stanislav <opelkia@ukr.net>
2023-10-27 22:04:54 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,9 @@
-// якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, функція має повертати рядок "Insufficient funds!"
-//в іншому випадку функція має повертати рядок "You ordered <quantity> drods worth <totalPrice> credits!",
-//де < quantity > це кількість замовлених дроїдів, а < totalPrice > це їх загальна вартість.
 
 "use strict";
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let totalPrice = quantity * pricePerDroid;  // рахуем загальну суму замовленя
-    if (totalPrice > customerCredits) {    //  перевіряем умову
-        return "Insufficient funds!";
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+function normalizeInput(input, to) {
+  to = "upper" ? input.toUpperCase : input.toLowerCase;
+  return input;
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+
+console.log(normalizeInput("This ISN'T SpaM", "lower"));

c560b014bd86d404c1c862da7d8996bbb82b03e2
Stanislav <opelkia@ukr.net>
2023-11-05 23:30:39 +0200

    cc

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,18 @@
-
 "use strict";
 function getLastElementMeta(array) {
 
-
     array[0] = array.length - 1;
+    // console.log(array[0]);
     array[1] = array[array.length - 1];
-    return array;
+    // console.log(array[1]);
+
+
+  for (let x = 0; x < array.length-1; x += 1) {
+    const element = array[x];
+      return array[0, 1];
 }
 
-console.log(getLastElementMeta(['apple','peach','pear' ,'bananna']));
-console.log(getLastElementMeta(["apple", "peach", "pear"]));
-console.log(getLastElementMeta(["apple", "peach"]));
-console.log(getLastElementMeta(["apple"]));
+    console.log(getLastElementMeta(["apple", "peach", "pear", "bananna"]));
+    console.log(getLastElementMeta(["apple", "peach", "pear"]));
+    console.log(getLastElementMeta(["apple", "peach"]));
+    console.log(getLastElementMeta(["apple"]));
